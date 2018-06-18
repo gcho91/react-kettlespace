ac666d58c2c81b5af613b57f9acab30bde85a7ed
gcho91 <gcho91@gmail.com>
2018-06-18 17:15:43 -0400

    Refactor JSX and CSS styling into components

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import '../styles/Header.css';
 
 var coworkingImg = require('../img/coworking.jpg');
 var coworkingImgSmall = require('../img/coworking-small.jpg');
+var kettleLogo = require('../img/kettle-logo.png');
+
 
 
 class Header extends React.Component {
     render() {
       return (
-        <div>
-        <div className="container header">
-          <h1>The easiest way to cowork</h1>
+        
+          <div className="container header">
+
+            <Menu />
+              <h1>The Easiest Way To Cowork</h1>
+            
+            <button className="try-btn">Try for a Week</button>
           </div>
-          <button>Fuck the trial</button>
-        </div>
+          
       );
+    }
+  }
+
+
+  {/* Menu component*/}
+
+  class Menu extends React.Component {
+    render(){
+      return (
+        <div className="navbar">
+          
+          <h3>OwO NAVBAR BITCHES</h3>
+        </div>
+      )
     }
   }
 
--- src/components/SubSectionComponent.js
+++ src/components/SubSectionComponent.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import '../styles/SubSection.css'
 
 class SubSection extends React.Component {
     render() {
       return (
         <div className="container subsection">
-          <h1>Subsection Component here</h1>
+          
+          <p>NYC's most convenient & collaborative network of workspaces → inside of restaurants during their downtime</p>
+
+<div className="sign-up-field">
+          <input type="email" name="email" placeholder="Your email address" required />
+          <input type="submit" value="Try for free"  />
+</div>
         </div>
       );
     }
